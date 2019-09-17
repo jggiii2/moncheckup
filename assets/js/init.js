31f595f012adde7a7f6d8b13eff3a4c3259b27c0
Sylvester Klirowski <klirowski.s@gmail.com>
2019-09-17 19:07:08 +0200

    version 2

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,8 +1,8 @@
-(function($){
-  $(function(){
+(function($) {
+  $(function() {
     $('.parallax').parallax();
     $('.sidenav').sidenav();
-    $(".dropdown-trigger").dropdown();
+    $('.dropdown-trigger').dropdown();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
@@ -10,21 +10,47 @@
 var countdownclock1 = document.querySelector('.countdown');
 var countdownclock2 = document.querySelector('.countdown2');
 
-if(countdownclock1){
-    const efcc_countdown = new countdown({
-        target: '.countdown',
-        dayWord: ' days',
-        hourWord: ' hours',
-        minWord: ' mins',
-        secWord: ' secs'
-    });
+if (countdownclock1) {
+  const efcc_countdown = new countdown({
+    target: '.countdown',
+    dayWord: ' days',
+    hourWord: ' hours',
+    minWord: ' mins',
+    secWord: ' secs'
+  });
 }
-if(countdownclock2){
-    const efcc_countdown2 = new countdown({
-        target: '.countdown2',
-        dayWord: ' days',
-        hourWord: ' hours',
-        minWord: ' mins',
-        secWord: ' secs'
-    });
+if (countdownclock2) {
+  const efcc_countdown2 = new countdown({
+    target: '.countdown2',
+    dayWord: ' days',
+    hourWord: ' hours',
+    minWord: ' mins',
+    secWord: ' secs'
+  });
 }
+
+/* MATERIALIZE SELECT INITIALIZATION */
+// Add DOBs to health assessment card
+function addDOBs() {
+  // populate the birth year options
+  d = new Date();
+  curr_year = d.getFullYear() - 11;
+  var dateOfBirth = document.querySelector('#birthyear');
+  for (var i = 0; i < 100; i++) {
+    var year = document.createElement('option');
+    year.textContent = curr_year - i;
+    dateOfBirth.appendChild(year);
+  }
+}
+addDOBs();
+// initilize select elements
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+});
+
+/* MATERIALIZE MODAL INITIALIZATION */
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
